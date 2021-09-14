a5e001a55858441d1bbf33f49e590a5bb0bca817
hidetomo0102 <hidesakura23>
2021-09-14 15:26:37 +0900

    created project

---
+++ src/context/shopContext.jsx
@@ -0,0 +1,72 @@
+import React from "react";
+import { Component } from "react";
+import Client from "shopify-buy";
+
+const ShopContext = React.createContext();
+
+const client = Client.buildClient({
+  domain: process.env.REACT_APP_SHOPIFY_API_TOKEN,
+  storefrontAccessToken: process.env.REACT_APP_SHOPIFY_DOMAIN,
+});
+
+class ShopProvider extends Component {
+  state = {
+    product: {},
+    products: [],
+    checkout: {},
+    isCartOpen: false,
+    isMenuOpen: false,
+  };
+
+  componentDidMount() {
+    if (localStorage.checkout_id) {
+      this.fetchCheckout(localStorage.checkout_id);
+    } else {
+      this.createCheckout();
+    }
+  }
+
+  createCheckout = async () => {
+    const checkout = await client.checkout.create();
+    localStorage.setItem("checkout_id", checkout.id);
+    this.setState({ checkout: checkout });
+  };
+
+  fetchCheckout = async (checkoutId) => {
+    client.checkout.fetch(checkoutId).then((checkout) => {
+      this.setState({ checkout: checkout });
+    });
+  };
+
+  addItemToCheckout = async () => {};
+
+  removeLineItem = async (lineItemIdsToRemove) => {};
+
+  fetchAllProducts = async () => {
+    const products = await client.product.fetchAll();
+    this.setState({ products: products });
+  };
+
+  fetchProductWithHandle = async (handle) => {
+    const product = await client.product.fetchByHandle(handle);
+    this.setState({ product: product });
+  };
+
+  closeCart = () => {};
+
+  openCart = () => {};
+
+  closeMenu = () => {};
+
+  openMenu = () => {};
+
+  render() {
+    return <ShopContext.Provider>{this.props.children}</ShopContext.Provider>;
+  }
+}
+
+const ShopConsumer = ShopContext.Consumer;
+
+export { ShopConsumer, ShopContext };
+
+export default ShopProvider;
