f3afc6f9514ad8b5dd5d32cff14e64bef25b968b
hidetomo0102 <hidesakura23>
2021-09-16 20:54:30 +0900

    Productページをスタイリング

--- src/pages/ProductPage.jsx
+++ src/pages/ProductPage.jsx
@@ -1,6 +1,14 @@
 import React, { useContext, useEffect } from "react";
 import { useParams } from "react-router";
-import { Box, Grid, Text, Button, Heading, Image } from "@chakra-ui/react";
+import {
+  Box,
+  Grid,
+  Text,
+  Button,
+  Heading,
+  Image,
+  Flex,
+} from "@chakra-ui/react";
 
 import { ShopContext } from "../context/shopContext";
 
@@ -16,17 +24,34 @@ export const ProductPage = () => {
 
   if (!product.title) return <div>Loading...</div>;
   return (
-    <Box>
-      <Grid gridTemplateColumns="repeat(2, 1fr)">
-        <Image src={product.images[0].src} />
-        <Box>
-          <Heading>{product.title}</Heading>
-          <Text>${product.variants[0].price}</Text>
-          <Text>${product.description}</Text>
-          <Button onClick={() => addItemToCheckout(product.variants[0].id, 1)}>
+    <Box p="2rem">
+      <Grid templateColumns={["repeat(1, 1fr)", "repeat(2, 1fr)"]} m="auto">
+        <Flex justifyContent="center" alignItems="center">
+          <Image src={product.images[0].src} />
+        </Flex>
+        <Flex
+          flexDirection="column"
+          alignItems="center"
+          justifyContent="center"
+          px="2rem"
+        >
+          <Heading pb="2rem">{product.title}</Heading>
+          <Text fontWeight="bold" pb="2rem">
+            ${product.variants[0].price}
+          </Text>
+          <Text color="gray.500" pb="2rem">
+            ${product.description}
+          </Text>
+          <Button
+            w="10rem"
+            backgroundColor="#ff38BD"
+            color="white"
+            _hover={{ opacity: "70%" }}
+            onClick={() => addItemToCheckout(product.variants[0].id, 1)}
+          >
             カートへ追加
           </Button>
-        </Box>
+        </Flex>
       </Grid>
     </Box>
   );
