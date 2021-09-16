589e38e8c912492219e7a87b6a058877698de76f
hidetomo0102 <hidesakura23>
2021-09-16 13:31:24 +0900

    カート機能を追加

--- src/components/Cart.jsx
+++ src/components/Cart.jsx
@@ -1,5 +1,6 @@
 import React, { useContext } from "react";
 import {
+  Box,
   Button,
   Drawer,
   DrawerBody,
@@ -8,7 +9,12 @@ import {
   DrawerOverlay,
   DrawerContent,
   DrawerCloseButton,
+  Grid,
+  Image,
+  Text,
+  Link,
 } from "@chakra-ui/react";
+import { CloseIcon } from "@chakra-ui/icons";
 import { ShopContext } from "../context/shopContext";
 
 export const Cart = () => {
@@ -22,10 +28,58 @@ export const Cart = () => {
           <DrawerCloseButton />
           <DrawerHeader>Your Shopping Cart</DrawerHeader>
 
-          <DrawerBody>Your Cart</DrawerBody>
+          <DrawerBody>
+            {checkout.lineItems &&
+              checkout.lineItems.map((item) => (
+                <Grid templateColumns="repeat(4, 1fr)" gap={1} key={item.id}>
+                  <Box
+                    display="flex"
+                    alignItems="center"
+                    justifyContent="center"
+                  >
+                    <CloseIcon
+                      cursor="pointer"
+                      onClick={() => removeLineItem(item.id)}
+                    />
+                  </Box>
+                  <Box
+                    display="flex"
+                    alignItems="center"
+                    justifyContent="center"
+                  >
+                    <Image src={item.variant.image.src} />
+                  </Box>
+                  <Box
+                    display="flex"
+                    flexDir="column"
+                    align="center"
+                    justify="center"
+                  >
+                    <Text fontSize="sm" fontWeight="bold">
+                      {item.title}
+                    </Text>
+                    <Text fontSize="sm">{item.variant.title}</Text>
+                  </Box>
+                  <Box>
+                    <Text
+                      height="100%"
+                      display="flex"
+                      align="center"
+                      justifyContent="center"
+                    >
+                      {item.variant.price}
+                    </Text>
+                  </Box>
+                </Grid>
+              ))}
+          </DrawerBody>
 
           <DrawerFooter>
-            <Button colorScheme="blue">Checkout</Button>
+            <Button w="100%" colorScheme="blue">
+              <Link w="100%" href={checkout?.webUrl}>
+                決済へ進む
+              </Link>
+            </Button>
           </DrawerFooter>
         </DrawerContent>
       </Drawer>
--- src/pages/ProductPage.jsx
+++ src/pages/ProductPage.jsx
@@ -1,11 +1,13 @@
 import React, { useContext, useEffect } from "react";
 import { useParams } from "react-router";
+import { Box, Grid, Text, Button, Heading, Image } from "@chakra-ui/react";
+
 import { ShopContext } from "../context/shopContext";
 
 export const ProductPage = () => {
   const { handle } = useParams();
 
-  const { fetchProductWithHandle, additemToCheckout, product } =
+  const { fetchProductWithHandle, addItemToCheckout, product } =
     useContext(ShopContext);
 
   useEffect(() => {
@@ -14,8 +16,18 @@ export const ProductPage = () => {
 
   if (!product.title) return <div>Loading...</div>;
   return (
-    <div>
-      <h1>{product.title}</h1>
-    </div>
+    <Box>
+      <Grid gridTemplateColumns="repeat(2, 1fr)">
+        <Image src={product.images[0].src} />
+        <Box>
+          <Heading>{product.title}</Heading>
+          <Text>${product.variants[0].price}</Text>
+          <Text>${product.description}</Text>
+          <Button onClick={() => addItemToCheckout(product.variants[0].id, 1)}>
+            カートへ追加
+          </Button>
+        </Box>
+      </Grid>
+    </Box>
   );
 };
