4a6ab07a432564827e61341436b14effd6c9c255
hidetomo0102 <hidesakura23>
2021-09-17 20:47:51 +0900

    テキスト画像付きセクションを追加

---
+++ src/components/ImageWithText.jsx
@@ -0,0 +1,35 @@
+import React from "react";
+import { Box, Flex, Button, Text, Image, Heading } from "@chakra-ui/react";
+
+export const ImageWithText = ({ button, reverse, image, text, heading }) => {
+  const reverseSection = reverse ? "row-reverse" : "row";
+
+  return (
+    <Box>
+      <Flex flexDir={["column", reverseSection]} w="100%">
+        <Image objectFit="cover" w={["100%", "50%"]} src={image} />
+        <Box
+          w={["100%", "50%"]}
+          display="flex"
+          flexDir="column"
+          justifyContent="center"
+          alignItems="center"
+          p="2rem"
+        >
+          <Heading pb="2rem">{heading && heading}</Heading>
+          <Text pb="2rem">{text && text}</Text>
+          {button ? (
+            <Button
+              w="10rem"
+              backgroundColor="#FF38BD"
+              color="white"
+              _hover={{ opacity: "70%" }}
+            >
+              チェックする
+            </Button>
+          ) : null}
+        </Box>
+      </Flex>
+    </Box>
+  );
+};
